ccd7e80fff468879908f070172fbbc913674a02a
GK <gpkmyworld@gmail.com>
2021-09-29 19:53:53 +0530

    lecture 2

---
+++ userInput.py
@@ -0,0 +1,8 @@
+# input
+# variable = datatype(input(msg))
+name  = input("Enter name : ")
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+
+print("Welcome ",name,num1," + ",num2,' = ',num1+num2)
