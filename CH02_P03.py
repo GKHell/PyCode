e8d310650ce0ca55cc1576520dc43478659719e9
GK <gpkmyworld@gmail.com>
2021-10-01 19:40:58 +0530

    01/10/2021⠀⎦⎡⠀19:40⠀⎦⎢⠀Friday

--- CH02_P03.py
+++ CH02_P03.py
@@ -6,7 +6,10 @@ b = int(input("Enter the number : "))
 
 if a>b:
 	print(a,"is max")
-
+else:
+	print(b,"is max")
 
 print("welcome .....")
 
+## TASK
+# write a program to check entered 2 numbers are same or not.
